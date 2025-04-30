a9ee138625c2c75b2daa039ff4219c436c4490ac
Iris <iris.m.gong@gmail.com>
2025-04-30 15:11:10 -0400

    organized CAD files and updated Arduino code

--- Arduino/microbot_multidirection_serial/microbot_multidirection_serial.ino
+++ Arduino/microbot_multidirection_serial/microbot_multidirection_serial.ino
@@ -1,11 +1,27 @@
-#define UDStepPin 2
-#define UDDirPin 3
-#define UDEnablePin 4
-#define LRStepPin 9
-#define LRDirPin 10
-#define LREnablePin 11
-#define speed 22 //higher speed means a lower ACTUAL speed of the motor default 20
-// LR is ROYB color jumpers, UD is GBVG
+// Interprets serial communication messages from MATLAB to send
+// commands to motor drivers
+
+/* Command interpretation */
+// each step in one of 8 directions is encoded as a char:
+// Vertical and horizontal movement represented by the four cardinal directions NSEW
+// diagonal movements: A is northwest, B is northeast, C is southeast, D is southwest
+// X (or other letters) represent default case (no movement) 
+// F represents full command set completion (used to terminate MATLAB program and 
+// safely disconnect Arduino)
+// Arduino uses Serial.read() to pop incoming letters
+
+/* Setup */
+// 1x8 header pins should be at the top right of the PCB board
+// see Github schematic: pins 1-4 control U/D movement, pins 5-8 control L/R movement
+
+#define UDStepPin 2     // pin to control up/down step timing
+#define UDDirPin 3      // direction indicator: N = LOW, S = HIGH
+#define UDEnablePin 4   // active low enable
+
+#define LRStepPin 9     // pin to control left/right step timing
+#define LRDirPin 10     // direction indicator: E = LOW, W = HIGH
+#define LREnablePin 11  // active low enable
+#define speed 30        // milliseconds between each L-H or H-L transition
 
 void setup() {
   pinMode(UDStepPin, OUTPUT);
@@ -23,7 +39,8 @@ void loop()
 {
   char direction;
   if (Serial.available() > 0) {
-    direction = Serial.read(); // Read the incoming byte
+    direction = Serial.read(); // Read the incoming byte into 'direction'
+
     // code always steps for both motors; 
     //however, if the enable pin is on, current will not be sent to the PCB, turning that direction off
     digitalWrite(UDEnablePin, LOW);
@@ -62,8 +79,9 @@ void loop()
         digitalWrite(LRDirPin, HIGH);
         break;
       case 'F':
+        // serial comm with Matlab to indicate Arduino can be disconnected
         Serial.println("Target reached");
-        Serial.flush();
+        Serial.flush(); 
       default:
         digitalWrite(UDEnablePin, HIGH);
         digitalWrite(LREnablePin, HIGH);
@@ -71,7 +89,7 @@ void loop()
     }
     digitalWrite(UDStepPin, HIGH);
     digitalWrite(LRStepPin, HIGH);
-    delay(speed); //speed of a single step.  
+    delay(speed); 
     digitalWrite(UDStepPin, LOW);
     digitalWrite(LRStepPin, LOW);
     delay(speed); 
